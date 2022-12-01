41993997a2c9a46dfd7e152a7e5b123eeba75e4a
artyomgonc-cfu <119576824+artyomgonc-cfu@users.noreply.github.com>
2022-12-01 15:59:57 +0300

    Update Lab1_2.3.cpp

--- Lab1_2.3.cpp
+++ Lab1_2.3.cpp
@@ -15,7 +15,33 @@ double accuracy() {
     return old;
 }
 
-double calc_cos(int x) {
+double calc_sin(double x) {
+
+    double fact = 1;
+    int counter = 1;
+
+    double ans = x;
+    double old_ans = 1000;
+    double acc = accuracy();
+
+    while (abs(ans-old_ans) > acc) {
+        old_ans = ans;
+        
+        fact = fact * 2 * counter * (2 * counter -1);
+        double loc_val = pow(x, 2*counter + 1) / (fact * (2 * counter + 1));
+        if (counter % 2 == 1) loc_val = -loc_val;
+
+        ans += loc_val;
+
+        counter++;
+
+        //cout << ans << endl;
+    }
+
+    return ans;
+}
+
+double calc_cos(double x) {
 
     double powv = 1;
     double fact = 1;
@@ -44,22 +70,54 @@ double calc_cos(int x) {
 
 int main() {
     int wid = 15;
-
+    cout << "Cosinus\n";
     cout << scientific;
     cout << setw(5) << "x" << '|' << setw(wid) << "calculated" << '|' << setw(wid) << "cos(x)" << '|'
-        << setw(wid) << "fault" << "|fault (%)" << endl;
+        << setw(wid) << "abs fault" << "| rel fault" << endl;
 
     vector<int> test_vals{0};
     for (int i = 5; i <= 50; i+=5) {
         test_vals.push_back(i);
-        test_vals.push_back(-i);
     }
     sort(test_vals.begin(), test_vals.end());
 
     for (int i : test_vals) {
         cout << setw(5) << i << '|' << setw(wid) << calc_cos(i) << '|' << setw(wid) << cos(i) << '|'
-        << setw(wid) << abs(calc_cos(i) - cos(i)) << '|' << fixed << abs(calc_cos(i) - cos(i))/abs(cos(i))*100 << "%" << endl;
+        << setw(wid) << abs(calc_cos(i) - cos(i)) << '|' << abs(calc_cos(i) - cos(i))/abs(cos(i)) << endl;
         cout << scientific;
+    }
+
+    cout << endl << "sinus\n";
+    cout << setw(5) << "x" << '|' << setw(wid) << "calculated" << '|' << setw(wid) << "sin(x)" << '|'
+        << setw(wid) << "abs fault" << "| rel fault" << endl;
+    for (int i : test_vals) {
+        cout << setw(5) << i << '|' << setw(wid) << calc_sin(i) << '|' << setw(wid) << sin(i) << '|'
+        << setw(wid) << abs(calc_sin(i) - sin(i)) << '|' << abs(calc_sin(i) - sin(i))/abs(sin(i)) << endl;
+        cout << scientific;
+    }
+
+    cout << "\nCorrected cosinus\n";
+    cout << setw(5) << "x" << '|' << setw(wid) << "calculated" << '|' << setw(wid) << "cos(x)" << '|'
+        << setw(wid) << "abs fault" << "| rel fault" << endl;
+
+    for (int j : test_vals) {
+        int factor = double(j) / (2*acos(-1));
+
+        double i = double(j) - (2*acos(-1))*factor;
+        cout << setw(5) << j << '|' << setw(wid) << calc_cos(i) << '|' << setw(wid) << cos(i) << '|'
+        << setw(wid) << abs(calc_cos(i) - cos(i)) << '|' << abs(calc_cos(i) - cos(i))/abs(cos(i)) << endl;
+    }
+
+    cout << "\nCorrected sinus\n";
+    cout << setw(5) << "x" << '|' << setw(wid) << "calculated" << '|' << setw(wid) << "sin(x)" << '|'
+        << setw(wid) << "abs fault" << "| rel fault" << endl;
+
+    for (int j : test_vals) {
+        int factor = double(j) / (2*acos(-1));
+
+        double i = double(j) - (2*acos(-1))*factor;
+        cout << setw(5) << j << '|' << setw(wid) << calc_sin(i) << '|' << setw(wid) << sin(i) << '|'
+        << setw(wid) << abs(calc_sin(i) - sin(i)) << '|' << abs(calc_sin(i) - sin(i))/abs(sin(i)) << endl;
     }
 
     return 0;
