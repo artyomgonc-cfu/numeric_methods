38a8c84a5031679dc711bb39fd4cc5d6a53ad857
artyomgonc-cfu <119576824+artyomgonc-cfu@users.noreply.github.com>
2022-12-02 14:27:14 +0300

    Update 4.cpp

--- Lab1/4.cpp
+++ Lab1/4.cpp
@@ -15,12 +15,30 @@ double l(int i) {
 }
 
 int main() {
-	
+
 	double da = 1e-7;
 
+	cout << setw(2) << "k" << '|' << setw(15) << "l(k)" << '|'
+		<< setw(15) << "dx(k)" << '|' << setw(15) << "x(k)" << endl;
 	for (int k = 1; k <= 20; k++) {
 		cout << setw(2) << k << '|' << scientific << setw(15) << l(k) << '|'
 			<< setw(15) << l(k) * da << '|' << setw(15) << double(k) + l(k) * da << endl;
+	}
+
+	int counter = -8;
+	for (da = da / 10; true; da /= 10, counter--) {
+		bool ok = true;
+		for (int k = 1; k <= 20; k++) {
+			double fault = abs(l(k) * da);
+			if ((k < 10 && fault > 5e-6) || (k >= 10 && fault > 5e-5)) {
+				ok = false;
+				break;
+			}
+		}
+		if (ok) {
+			cout << "Allowed da = " << setprecision(0) << da << endl;
+			break;
+		}
 	}
 
 	return 0;
