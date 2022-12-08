7bfed297efd0bd374ae9ac652bfe4aed0fc554d1
artyomgonc-cfu <119576824+artyomgonc-cfu@users.noreply.github.com>
2022-12-08 13:22:11 +0300

    Update 1.1.cpp

--- Lab1/1.1.cpp
+++ Lab1/1.1.cpp
@@ -9,6 +9,14 @@ int main() {
         e = e/2.0;
     }
     cout << old << endl;
+    
+    float e1 = 1.0;
+    float old1;
+    while (e1 > 0.0) {
+        old1 = e1;
+        e1 = e1/2.0;
+    }
+    cout << old1 << endl;
 
     return 0;
 }
