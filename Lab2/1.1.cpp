e162865eb031b5269c41302b889183238b8e2a8b
artyomgonc-cfu <119576824+artyomgonc-cfu@users.noreply.github.com>
2022-12-08 14:38:15 +0300

    Create 1.1.cpp

---
+++ Lab2/1.1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+double f(double val, const vector<double>& x, const vector<double>& y) {
+    double ans = 0;
+    for(int i = 0; i < y.size(); i++) {
+        double factor = 1;
+        for (int j = 0; j < x.size(); j++)
+            if (j!=i) {
+                factor *= (val-x[j])/(x[i]-x[j]);
+            }
+        ans += factor*y[i];
+    }
+    return ans;
+}
+
+int main() {
+    vector<double> x{-0.65, -0.531, -0.2, 0.223, 1};    
+    vector<double> y{0.71, 0.322, 0.5, 1, 0.28}; 
+
+    cout << f(-0.2, x, y) << endl;
+    cout << f(0, x, y) << endl;
+    cout << f(0.2, x, y) << endl;
+
+    return 0;
+}
