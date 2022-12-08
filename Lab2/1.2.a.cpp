59c263b3a7aa17ebcbcef4dc4120072afe4995a3
artyomgonc-cfu <119576824+artyomgonc-cfu@users.noreply.github.com>
2022-12-08 15:55:43 +0300

    Create 1.2.a.cpp

---
+++ Lab2/1.2.a.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+double l(double val, const vector<double>& x, const vector<double>& y) {
+    double ans = 0;
+    for(int i = 0; i < y.size(); i++) {
+        double factor = 1;
+        for (int j = 0; j < x.size(); j++)
+            if (j!=i) {
+                factor *= (val-x[j])/(x[i]-x[j]);
+            }
+        ans += factor*y[i];
+    }
+    return ans;
+}
+
+double f(double x) {
+    return 1.0/(1.0 + 16.0*x*x);
+}
+
+int main() { 
+    
+    vector<int> nv{4, 10, 20};
+    for (int n : nv) {
+        cout << "n=" << n << endl;
+
+        vector<double> x;    
+        vector<double> y;
+        for (double xv = -1; xv <= 1; xv+=2.0/n) {
+            x.push_back(xv);
+            y.push_back(f(xv));
+        }
+
+        cout << "x:\n";
+        for (double xv : x)
+            cout << xv <<endl;
+
+        cout << "y:\n";
+        for (double yv : y)
+            cout << yv <<endl;
+    }
+    return 0;
+}
